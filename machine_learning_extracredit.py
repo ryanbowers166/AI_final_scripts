af8cd2c1e55ccd280ac8f08a9ed684256d349ae3
ryanbowers166 <ryanbowers166@gmail.com>
2024-12-08 11:52:36 -0600

    Update machine_learning_extracredit.py

--- machine_learning_extracredit.py
+++ machine_learning_extracredit.py
@@ -9,11 +9,4 @@ def Q11_1():
     num_params = params_layer1+params_layer2+params_layer3+params_output
     print(num_params)
 
-def Q11_3():
-    params_conv1 = (5*5*3 + 1)*32     # (k_w \times k_h \times C_{in} +1)\times C_{out}
-    params_pool1 = 0
-    params_conv2 = (3*3*3 + 1)*16
-    params_pool2 = 0
-    params_linear1 =
-
 Q11_1()
